63f6ec6d337c2212da226f0a6d81dfee2db71bd8
louis-ngugi <louisngugi858@gmail.com>
2025-04-06 19:33:09 +0300

    create a guest model

--- models.py
+++ models.py
@@ -17,3 +17,19 @@ class Episode(db.Model):
             "appearances": [appearance.to_dict() for appearance in self.appearances]
         }
     
+class Guest(db.Model):
+    __tablename__ = 'guests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    occupation = db.Column(db.String, nullable=False)
+
+    appearances = db.relationship('Appearance', backref='guest', cascade='all, delete-orphan')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "occupation": self.occupation
+        }
+    
