ff637661b290720c4f620e9bf7e1713b0d364877
louis-ngugi <louisngugi858@gmail.com>
2025-04-06 19:31:09 +0300

    create an episode Model

---
+++ models.py
@@ -0,0 +1,19 @@
+
+
+class Episode(db.Model):
+    __tablename__ = 'episodes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String, nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+
+    appearances = db.relationship('Apperance', backref='episode', cascade='all, delete-orphan')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "date": self.date,
+            "number": self.number,
+            "appearances": [appearance.to_dict() for appearance in self.appearances]
+        }
+    
