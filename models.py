a939de80d062b102e610ca1812da8146f7f0abef
louis-ngugi <louisngugi858@gmail.com>
2025-04-06 19:35:37 +0300

    create class Appearance

--- models.py
+++ models.py
@@ -33,3 +33,26 @@ class Guest(db.Model):
             "occupation": self.occupation
         }
     
+class Appearance(db.Model):
+    __tablename__ = 'appearances'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "rating": self.rating,
+            "episode_id": self.episode_id,
+            "guest_id": self.guest_id,
+            "episode": self.episode.to_dict(),
+            "guest": self.guest.to_dict()
+        }
+    
+    @validates('rating')
+    def validate_rating(self, key, rating):
+        if rating < 1 or rating > 5:
+            raise ValueError("rating must be between 1 and 5")
+        return rating
